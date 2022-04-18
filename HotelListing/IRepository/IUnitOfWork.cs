f8f952d0d86bb121006a5db2c95830a680cbf20c
Vasile-Bogdan Spaller <vspaller45@massmutual.com>
2022-04-18 23:33:35 +0300

    Creation of generic Repositories>

---
+++ HotelListing/IRepository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using HotelListing.Data;
+
+namespace HotelListing.IRepository
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+        Task Save();
+    }
+}
